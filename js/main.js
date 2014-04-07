8df0beca17dc3aa228c55715d6af8b7fb99117ce
JWAccent <John.Wright@accent-inc.com>
2014-04-07 15:02:20 -0700

    Added alert code.

--- js/main.js
+++ js/main.js
@@ -13,11 +13,21 @@ var app = {
         });
     },
 
-    initialize: function() {
-        this.store = new WebSqlStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
-
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
+	
+	initialize: function() {
+		var self = this;
+		this.store = new WebSqlStore(function() {
+			self.showAlert('Store Initialized', 'Info');
+		});
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	}
 };
 
 app.initialize();
