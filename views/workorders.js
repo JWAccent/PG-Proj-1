446e368e8c3e2ab39f88ebe50fc2493a5f7b7092
JWAccent <John.Wright@accent-inc.com>
2014-04-14 14:25:52 -0700

    Removed alerts

--- views/workorders.js
+++ views/workorders.js
@@ -1,14 +1,5 @@
 ﻿MyApp.workorders = function (params) {      
         
-    var workOrder = new MyApp.models.WorkOrder;
- 	var works;
-    workOrder.getAllWorkOrders(false, function (workOrders) {
-    	works = workOrders;
-    	alert(works[0].Id)
-    	alert(works[0].WorkOrderId)
-    });
-
-
     var viewModel = {
        	dataSource: new DevExpress.data.DataSource({
             load: function(loadOptions) {
